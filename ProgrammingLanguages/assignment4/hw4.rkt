4966d9a1d168626378ab4625bc34bfcb5fb592b9
AlexMost <alexmost1989@gmail.com>
2014-11-08 16:37:33 +0200

    ProgrammingLanguages assignment4 first 5 exercises

---
+++ ProgrammingLanguages/assignment4/hw4.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+(provide (all-defined-out)) ;; so we can put tests in a second file
+;; put your code below
+
+;; (1)
+(define (sequence low high stride)
+  (if (> low high) 
+      '()
+      (cons low (sequence (+ low stride) high stride))))
+
+;; (2)
+(define (string-append-map xs suffix)
+  (map (lambda (x) (string-append x suffix)) xs))
+
+;; (3)
+(define (list-nth-mod xs n)
+  (cond [(< n 0) (error "list-nth-mod: negative number")]
+        [(= n 0) (error "list-nth-mod: empty list")]
+        [#t (car (list-tail xs (remainder n (length xs))))]))
+
+;; (4)
+(define (stream-for-n-steps s n)
+  (if (<= n 0)
+      '()
+      (cons (car (s)) (stream-for-n-steps (cdr (s)) (- n 1)))))
+
+;; (5)
+(define funny-number-stream
+  (letrec ([f (lambda (x)
+                (if (= (remainder x 5) 0)
+                   (cons (- x) (lambda () (f (+ 1 x))))
+                   (cons x (lambda () (f (+ 1 x))))))])
+  (lambda () (f 1))))
+
+;; (5)
+(define dan-then-dog
+  (letrec ([dog "dog.jpg"]
+           [dan "dan.jpg"]
+           [f (lambda (x)
+                (if (equal? x dan)
+                   (cons dan (lambda () (f dog)))
+                   (cons dog (lambda () (f dan)))))])
+  (lambda () (f dan))))
+
+;; (6)
+
+
