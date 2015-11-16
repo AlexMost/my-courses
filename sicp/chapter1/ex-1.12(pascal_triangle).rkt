5c94fafca79625a8b37358699d004c22607d84f1
Alexander Mostovenko <alexmost1989@gmail.com>
2015-11-17 00:42:17 +0200

    added Pascal triangle example

---
+++ sicp/chapter1/ex-1.12(pascal_triangle).rkt
@@ -0,0 +1,7 @@
+#lang racket
+(define (pascal-triangle row col) 
+   (cond ((> col row) 0) 
+         ((< col 0) 0) 
+         ((= col 1) 1) 
+         ((+ (pascal-triangle (- row 1) (- col 1)) 
+             (pascal-triangle (- row 1) col)))))
