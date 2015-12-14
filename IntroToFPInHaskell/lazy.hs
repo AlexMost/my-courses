4f2e78e2eae457447fedb0ce3a293741266efdef
Alexander Mostovenko <alexmost1989@gmail.com>
2015-12-15 00:27:16 +0200

    little cleanup for lazy example

--- IntroToFPInHaskell/lazy.hs
+++ IntroToFPInHaskell/lazy.hs
@@ -6,6 +6,7 @@ someDirtyWork = [fibs !! 5000000, fibs !! 500000]
 
 main :: IO ()
 main = do
-  [f1, f2] <- return someDirtyWork
-  let rr = show $ 2 * f1 + 2 * f2
+  let
+    [f1, f2] = someDirtyWork
+    rr = show $ 2 * f1 + 2 * f2
   print $ show $ length rr
