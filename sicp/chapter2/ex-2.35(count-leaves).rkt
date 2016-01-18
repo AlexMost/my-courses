2509d1a49acf4a3ee9db9ea05a5e1bb75636cc80
Alexander Mostovenko <alexmost1989@gmail.com>
2016-01-19 00:27:35 +0200

    added another version of count leaves func (using only reduce function)

--- sicp/chapter2/ex-2.35(count-leaves).rkt
+++ sicp/chapter2/ex-2.35(count-leaves).rkt
@@ -9,5 +9,11 @@
 (define (count-leaves t)
   (accumulate + 0
               (map (lambda (node) (if (pair? node) (count-leaves node) 1)) t)))
+
+
+(define (count-leaves-2 t)
+  (accumulate
+   (lambda (l1 l2) (if (pair? l1) (+ (count-leaves-2 l1) l2) (+ 1 l2)))
+   0 t))
                          
 
