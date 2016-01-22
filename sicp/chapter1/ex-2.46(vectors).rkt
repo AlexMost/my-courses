57517003248d9c68d1758495cc1b08d33496ba94
Alexander Mostovenko <alexmost1989@gmail.com>
2016-01-22 23:02:37 +0200

    ex-2.46 sicp

---
+++ sicp/chapter1/ex-2.46(vectors).rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(define (make-vect x y) (cons x y))
+
+(define (xcor-vect v) (car v))
+
+(define (ycor-vect v) (cdr v))
+
+(define (add-vect v1 v2)
+  (let ([x (+ (xcor-vect v1) (xcor-vect v2))]
+        [y (+ (ycor-vect v1) (ycor-vect v2))])
+    (make-vect x y)))
+
+(define (sub-vect v1 v2)
+  (let ([x (- (xcor-vect v1) (xcor-vect v2))]
+        [y (- (ycor-vect v1) (ycor-vect v2))])
+    (make-vect x y)))
+
+(define (scale-vect v s)
+  (make-vect (* (xcor-vect v) s) (* (ycor-vect v) s)))
