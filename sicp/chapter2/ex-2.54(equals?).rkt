27b7ecf50f38024551962dbb653a676c085b16e7
Alexander Mostovenko <alexmost1989@gmail.com>
2016-01-26 00:03:37 +0200

    added ex-2.54(equals?)

---
+++ sicp/chapter2/ex-2.54(equals?).rkt
@@ -0,0 +1,11 @@
+#lang racket
+(define (equal? l1 l2)
+  (cond [(and (null? l1) (null? l2)) #t]
+        [(or (null? l1) (null? l2)) #f]
+        [else (and (eq? (car l1) (car l2)) (equal? (cdr l1) (cdr l2)))]))
+
+;> (equal? '(this is a list) '(this is a list))
+;#t
+;> (equal? '(this is a list) '(this (is a) list))
+;#f
+;>
