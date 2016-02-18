22de5fb12b5aaefc72c583f301e23bd46951da73
Alexander Mostovenko <alexmost1989@gmail.com>
2016-02-18 22:44:22 +0200

    first rust program

---
+++ rust-lang-book/guessing_game/src/main.rs
@@ -0,0 +1,35 @@
+extern crate rand;
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    loop {
+        println!("Guess the number");
+
+        let secret_number = rand::thread_rng().gen_range(1, 101);
+
+        println!("Please input your guess");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("To small"),
+            Ordering::Greater => println!("To big"),
+            Ordering::Equal => {
+                println!("You win!!!");
+                break;
+            }
+        }
+    }
+}
