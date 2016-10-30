34abffa046c1bab60d52f4e317347979e285a1b5
Alexander Mostovenko <alexmost1989@gmail.com>
2016-10-30 18:32:40 +0200

    1.14

---
+++ CProgrammingLanguage/chapter1/1.14.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define SPACE ' '
+#define TAB '\t'
+#define NL '\n'
+#define IN 1
+#define OUT 0
+#define HIST_X 10
+#define HIST_Y 10
+
+
+int main() {
+	int c, i, j, range, tabs, nls, spaces;
+	range = 'z' - 'a';
+	int letters[range];
+
+	for (c = 0; c < range; c++)
+		letters[c] = 0;
+
+	while ((c = getchar()) != EOF) {
+		if ((c - 'a') >= range)
+			continue;
+		letters[c - 'a']++;
+	}
+
+	// printing words counts stats
+	printf("Histogram:\n");
+
+	// printing histogram
+	i = HIST_Y;
+	for (i = HIST_Y; i > 0; i--) {
+		for(j = 0; j < range; j++) {
+			if (letters[j] >= i) {
+				printf("%d ", 1);
+			} else {
+				printf("  ");
+			}
+		}
+		printf("\n");
+	}
+	
+	for (i = 0; i <= range; i++) {
+		printf("%c ", i + 'a');
+	}
+
+	return(0);
+}
