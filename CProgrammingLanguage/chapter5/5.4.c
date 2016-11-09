0bb5ad7a794dbecb7a74ecabaa654ffab4a9592d
Alexander Mostovenko <alexmost1989@gmail.com>
2016-11-10 00:55:57 +0200

    improvement for 5.4

--- CProgrammingLanguage/chapter5/5.4.c
+++ CProgrammingLanguage/chapter5/5.4.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 
-int strend(char *s1, char *s2) {
+int strend(const char *s1, const char *s2) {
 	int len = 0;
-	while(s2[len] != '\0')
-		len++;
-
-	while(*s1 != '\0')
-		s1++;
-
+	while(s2[len++] != '\0');
+	while(*s1++ != '\0');
 	s1 -= len;
 	while (*s1 == *s2) {
 		s1++;
 		s2++;
 	}
-	
-	if ((*(s1 - 1) == '\0') && (*(s2 - 1) == '\0'))
-		return 1;
-	return 0;
+	return ((*(s1 - 1) == '\0') && (*(s2 - 1) == '\0'));
 }
 
 int main() {
@@ -28,6 +21,6 @@ int main() {
 	} else {
 		printf("doesn't contains string\n");
 	}
-	
+
 	return 0;
 }
