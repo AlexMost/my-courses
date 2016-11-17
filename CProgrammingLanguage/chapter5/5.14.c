99d34fc652aaf6849f2d62d33f21ad22e414481f
Alexander Mostovenko <alexmost1989@gmail.com>
2016-11-18 00:08:17 +0200

    5.14

--- CProgrammingLanguage/chapter5/5.14.c
+++ CProgrammingLanguage/chapter5/5.14.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+
 void swap(char s[], int i, int j);
-void qsort(char s[], int left, int right);
+void qsort(char s[], int left, int right, int (*comp)(char, char));
+int asc(char s1, char s2);
+int desc(char s1, char s2);
 
 int main() {
-	char arr[] = {'6', '3', '4', '1', '5', '2'};
-	qsort(arr, 0, sizeof(arr) - 1);
+	char arr[] = "634152";
+	qsort(arr, 0, sizeof(arr) - 1, desc);
 	printf("%s\n", arr);
 	return 0;
 }
@@ -16,18 +20,26 @@ void swap(char s[], int i, int j) {
 	s[j] = tmp;
 }
 
-void qsort(char s[], int left, int right) {
+void qsort(char s[], int left, int right, int (*comp)(char, char)) {
 	int i, last;
 	if (left >= right)
 		return;
 	swap(s, left, (left + right) / 2);
 	last = left;
 	for (i = left + 1; i <= right; i++) {
-		if (s[i] < s[left]){
+		if (comp(s[i], s[left]) < 0){
 			swap(s, ++last, i);
 		}
 	}
 	swap(s, left, last);
-	qsort(s, left, last - 1);
-	qsort(s, last + 1, right);
+	qsort(s, left, (last - 1), comp);
+	qsort(s, last + 1, right, comp);
+}
+
+int asc(char s1, char s2) {
+	return s1 < s2 ? -1 : 1;
+}
+
+int desc(char s1, char s2) {
+	return s1 < s2 ? 1 : -1;
 }
