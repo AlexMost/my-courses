98764733a15b549dbc2b4ca63744a298f26b1bd9
Alexander Mostovenko <alexmost1989@gmail.com>
2017-07-20 23:20:58 +0300

    implemented mul

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,24 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+	@R2
+	M=0
+
+(LOOP)
+	
+	@R0
+	D=M     // R2 = R1 + R2
+	@R2
+	M=M+D
+
+	@R1
+	M=M-1
+	D=M
+
+	@LOOP
+	D;JNE
+
+(END)
+	@END
+	0;JMP
