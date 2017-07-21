5faa0e65fc481247320132b4c2c111c0c3ff506e
Alexander Mostovenko <alexmost1989@gmail.com>
2017-07-22 00:35:25 +0300

    implemented fill

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -12,3 +12,84 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+@0
+D=A  // D=0
+
+@color
+M=-1
+
+(KBDLOOP)
+	@KBD
+	D=M
+
+	@BLACK
+	D;JNE
+
+	@WHITE
+	0;JMP
+
+(WHITE)
+	@color
+	M=0
+
+	@FILL
+	0;JMP
+
+(BLACK)
+	@color
+	M=-1
+
+	@FILL
+	0;JMP
+
+(FILL)
+	@SCREEN
+	D=A
+	@scrn  // save screen offset
+	M=D
+
+	@32
+	D=A
+	@col    // col = 32
+	M=D
+
+	@256
+	D=A
+	@row    // row = 256
+	M=D
+
+	(ROW)
+		(COLUMN)
+			@color
+			D=M
+
+			@scrn   // scr = -1
+			A=M
+			M=D
+
+			@scrn   // scr = scr + 1
+			M=M+1
+
+			@col
+			M=M-1
+			D=M
+
+			@COLUMN
+			D;JNE
+
+		@32
+		D=A
+		@col  // col = 32
+		M=D
+
+		@row
+		M=M-1 // row = row - 1
+		D=M
+
+		@ROW
+		D;JNE
+
+		@KBDLOOP
+		0;JMP
