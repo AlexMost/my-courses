266ebde996cb9993aa591d2ca36d551956b37367
Alexander Mostovenko <alexmost1989@gmail.com>
2017-08-13 22:33:47 +0300

    vm in action

---
+++ nand2tetris/projects/07/MemoryAccess/BasicTest/BasicTest.asm
@@ -0,0 +1,262 @@
+// push constant 10
+@10
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop local 0
+@0
+D=A
+@LCL
+D=M+D
+@POPTMP
+M=D
+@SP
+M=M-1
+@SP
+A=M
+D=M
+@POPTMP
+A=M
+M=D
+// push constant 21
+@21
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 22
+@22
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop argument 2
+@2
+D=A
+@ARG
+D=M+D
+@POPTMP
+M=D
+@SP
+M=M-1
+@SP
+A=M
+D=M
+@POPTMP
+A=M
+M=D
+// pop argument 1
+@1
+D=A
+@ARG
+D=M+D
+@POPTMP
+M=D
+@SP
+M=M-1
+@SP
+A=M
+D=M
+@POPTMP
+A=M
+M=D
+// push constant 36
+@36
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop this 6
+@6
+D=A
+@THIS
+D=M+D
+@POPTMP
+M=D
+@SP
+M=M-1
+@SP
+A=M
+D=M
+@POPTMP
+A=M
+M=D
+// push constant 42
+@42
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 45
+@45
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop that 5
+@5
+D=A
+@THAT
+D=M+D
+@POPTMP
+M=D
+@SP
+M=M-1
+@SP
+A=M
+D=M
+@POPTMP
+A=M
+M=D
+// pop that 2
+@2
+D=A
+@THAT
+D=M+D
+@POPTMP
+M=D
+@SP
+M=M-1
+@SP
+A=M
+D=M
+@POPTMP
+A=M
+M=D
+// push constant 510
+@510
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// pop temp 6
+@SP
+M=M-1
+@SP
+A=M
+D=M
+@R11
+M=D
+// push local 0
+@0
+D=A
+@LCL
+A=M
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push that 5
+@5
+D=A
+@THAT
+A=M
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+M=M-1
+@SP
+A=M
+D=M
+A=A-1
+M=M+D
+// push argument 1
+@1
+D=A
+@ARG
+A=M
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// sub
+@SP
+M=M-1
+@SP
+A=M
+D=M
+A=A-1
+M=M-D
+// push this 6
+@6
+D=A
+@THIS
+A=M
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push this 6
+@6
+D=A
+@THIS
+A=M
+A=D+A
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+M=M-1
+@SP
+A=M
+D=M
+A=A-1
+M=M+D
+// sub
+@SP
+M=M-1
+@SP
+A=M
+D=M
+A=A-1
+M=M-D
+// push temp 6
+@R11
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+M=M-1
+@SP
+A=M
+D=M
+A=A-1
+M=M+D
