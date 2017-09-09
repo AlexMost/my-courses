89d7bbb61ceb6615158117a750b30dfc3669ff1f
Alexander Mostovenko <alexmost1989@gmail.com>
2017-09-09 22:55:16 +0300

    computed properties for let implemented

--- nand2tetris/compiler/parser/letStatement.js
+++ nand2tetris/compiler/parser/letStatement.js
@@ -5,17 +5,40 @@ const parseExpression = require('./expression');
 const parseVarname = require('./varName');
 
 function parse(tokenizer) {
+	const children = []
 	const letKeyword = tokenizer.next();
 	validateKeyword(letKeyword, KEYWORDS.let);
+	children.push(letKeyword);
+
 	const varName = parseVarname(tokenizer);
-	const eqOp = tokenizer.next();
+	children.push(varName);
+
+	let eqOp = tokenizer.next();
+
+	// computed property
+	if (eqOp.getValue() === '[') {
+		children.push(eqOp);
+
+		const compExpr = parseExpression(tokenizer);
+		children.push(compExpr);
+
+		const closingGap = tokenizer.next();
+		validateSymbol(closingGap, ']');
+		children.push(closingGap);
+
+		eqOp = tokenizer.next();
+	}
+
 	validateSymbol(eqOp, '=');
+	children.push(eqOp);
+
 	const sourceExpr = parseExpression(tokenizer);
+	children.push(sourceExpr);
+
 	const semi = tokenizer.next();
 	validateSymbol(semi, ';');
-	const children = [
-		letKeyword, varName, eqOp, sourceExpr, semi
-	]
+	children.push(semi);
+
 	return new ASTNode('letStatement', children);
 }
 
--- nand2tetris/compiler/tests/parser/test_letStatement.js
+++ nand2tetris/compiler/tests/parser/test_letStatement.js
@@ -16,11 +16,38 @@ const expectedSimple =
 <symbol> ; </symbol>
 </letStatement>`;
 
+const expectedComputed =
+`<letStatement>
+<keyword> let </keyword>
+<identifier> a </identifier>
+<symbol> [ </symbol>
+<expression>
+<term>
+<identifier> i </identifier>
+</term>
+</expression>
+<symbol> ] </symbol>
+<symbol> = </symbol>
+<expression>
+<term>
+<identifier> j </identifier>
+</term>
+</expression>
+<symbol> ; </symbol>
+</letStatement>`
+
 describe('parser letStatement', () => {
     it('should parse simple statement', () => {
-        const input = `let size = size;`;
+        const input = 'let size = size;';
         const tokenizer = new Tokenizer(input);
         const astNode = parseVarDec(tokenizer);
         expect(astNode.toXML()).to.eql(expectedSimple);
+    });
+
+    it('should parse computed properties', () => {
+        const input = 'let a[i] = j;';
+        const tokenizer = new Tokenizer(input);
+        const astNode = parseVarDec(tokenizer);
+        expect(astNode.toXML()).to.eql(expectedComputed);
     });
 });
