44f5538b72974566d8c2d96633361ad218d19c71
Alexander Mostovenko <alexmost1989@gmail.com>
2017-09-12 01:03:33 +0300

    modify analyzer

--- nand2tetris/compiler/JackAnalyzer.js
+++ nand2tetris/compiler/JackAnalyzer.js
@@ -1,20 +1,21 @@
 /* eslint no-console: 0 */
+const EOL = require('os').EOL;
 const fs = require('fs');
 // const path = require('path');
 const { Tokenizer } = require('./tokenizer');
 const parseClass = require('./parser/class');
 
-function readFile(filepath) {
-    return fs.readFileSync(filepath).toString();
+function processFile(filepath) {
+    const destFilepath = filepath.replace('.jack', '.xml');
+    const raw = fs.readFileSync(filepath).toString();
+    const tokenizer = new Tokenizer(raw);
+    const ast = parseClass(tokenizer);
+    fs.writeFileSync(destFilepath, `${ast.toXML()}${EOL}`);
+    console.log(`Saved result to ${destFilepath}`);
 }
 
 function main(entryPath) {
-    const destFilepath = entryPath.replace('.jack', '.xml');
-    const raw = readFile(entryPath);
-    const tokenizer = new Tokenizer(raw);
-    const ast = parseClass(tokenizer);
-    fs.writeFileSync(destFilepath, ast.toXML());
-    console.log(`Saved result to ${destFilepath}`);
+    processFile(entryPath);
 }
 
 main(process.argv[2]);
