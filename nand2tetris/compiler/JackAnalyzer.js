e80653cd98e2d5fa989eb34604f9d80b466e6f48
Alexander Mostovenko <alexmost1989@gmail.com>
2017-09-13 00:01:05 +0300

    added possibility to analyze dirs

--- nand2tetris/compiler/JackAnalyzer.js
+++ nand2tetris/compiler/JackAnalyzer.js
@@ -1,6 +1,7 @@
 /* eslint no-console: 0 */
 const EOL = require('os').EOL;
 const fs = require('fs');
+const path = require('path');
 const parseAST = require('./analyzer');
 
 function processFile(filepath) {
@@ -11,8 +12,23 @@ function processFile(filepath) {
     console.log(`Saved result to ${destFilepath}`);
 }
 
+
+function processDir(dirpath) {
+    fs.readdirSync(dirpath)
+    .filter((f) => fs.lstatSync(path.resolve(dirpath, f)).isFile())
+    .filter((f) => f.endsWith('.jack'))
+    .forEach((f) => processFile(path.resolve(dirpath, f)));
+}
+
+
 function main(entryPath) {
-    processFile(entryPath);
+    const lstat = fs.lstatSync(entryPath);
+    if(lstat.isFile()) {
+        processFile(entryPath);    
+    } else {
+        processDir(entryPath);
+    }
+    
 }
 
 main(process.argv[2]);
