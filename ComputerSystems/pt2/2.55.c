56082367de03ddc963d9f46d273b91d94d3d3e05
Alexander Mostovenko <alexmost1989@gmail.com>
2018-07-05 00:40:55 +0300

    2.55 exercise

--- ComputerSystems/pt2/2.55.c
+++ ComputerSystems/pt2/2.55.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+typedef unsigned char* byte_pointer;
+
+void show_bytes(byte_pointer start, int size) {
+  int i;
+  for (i = 0; i < size; i++) {
+    printf(" %.2x", start[i]);
+  }
+  printf("\n");
+}
+
+void show_int(int x) {
+  show_bytes((byte_pointer) &x, sizeof(x));
+}
+
 int main() {
-  printf("Hello again C");
+  show_int(12345);
   return 0;
 }
