a5550caa8056009c95d0225f27de12a45b56aa39
Alexander Mostovenko <alexmost1989@gmail.com>
2018-07-06 01:05:14 +0300

    use unsigned instead of unsigned int

--- ComputerSystems/ch2/2.59.c
+++ ComputerSystems/ch2/2.59.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <assert.h>
 
-int swap_least_byte(int x, int y) {
+unsigned swap_least_byte(unsigned x, unsigned y) {
   return (y & ~0xFF) | (x & 0xFF);
 }
 
 void run_259() {
-  int x = 0x89abcdef;
-  int y = 0x76543210;
+  unsigned x = 0x89abcdef;
+  unsigned y = 0x76543210;
   assert(swap_least_byte(x, y) == 0x765432ef);
 }
