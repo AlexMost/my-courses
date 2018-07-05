3ce90682f24b3ad7f01d0535fc12632e0fc5b670
Alexander Mostovenko <alexmost1989@gmail.com>
2018-07-06 00:50:58 +0300

    2.59

--- ComputerSystems/ch2/2.5.c
+++ ComputerSystems/ch2/2.5.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 void run_255();
 void run_258();
+void run_259();
 
 int main() {
   run_255();
   run_258();
+  run_259();
   return 0;
 }
---
+++ ComputerSystems/ch2/2.59.c
@@ -0,0 +1,12 @@
+#include <stdio.h>;
+
+int swap_least_byte(int x, int y) {
+  return (y & 0xFFFFFF00) + (x & 0x000000FF);
+}
+
+void run_259() {
+  int x = 0x89abcdef;
+  int y = 0x76543210;
+  printf("\nexersize 2.59\n");
+  printf("%x\n", swap_least_byte(x, y));
+}
